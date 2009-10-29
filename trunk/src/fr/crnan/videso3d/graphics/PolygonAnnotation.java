fbe357da9306bcea31cd2092faaa5cdbdefcad64
spyckerelle.bruno@gmail.com <spyckerelle.bruno@gmail.com@b624ef8a-b9c0-11de-b017-a9a2c7051f87>
2009-10-29 22:07:30 +0000

    Affiche les infos des carrés des mosaïques en annotation au survol par la souris. Possibilité de changer la couleur des carrés des mosaïques même en 2D. Fixes issue 28.

---
+++ trunk/src/fr/crnan/videso3d/graphics/PolygonAnnotation.java
@@ -0,0 +1,35 @@
+package fr.crnan.videso3d.graphics;
+
+import java.util.List;
+
+import fr.crnan.videso3d.geom.LatLonCautra;
+import gov.nasa.worldwind.geom.Position;
+import gov.nasa.worldwind.render.GlobeAnnotation;
+import gov.nasa.worldwind.render.airspaces.Polygon;
+/**
+ * Polygon avec Annotation intégrée
+ * @author Bruno Spyckerelle
+ * @version 0.1
+ */
+public class PolygonAnnotation extends Polygon {
+
+	private GlobeAnnotation annotation;
+
+	public PolygonAnnotation(List<LatLonCautra> locations) {
+		super(locations);
+	}
+
+	public void setAnnotation(String text){
+		if(annotation == null) {
+			annotation = new GlobeAnnotation(text, Position.ZERO);
+		} else {
+			annotation.setText(text);
+		}
+	}
+	
+	public GlobeAnnotation getAnnotation(Position pos){
+		annotation.setPosition(pos);
+		return annotation;
+	}
+	
+}
