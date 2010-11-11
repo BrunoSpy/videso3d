68ceff3729ba89039466e72c5eaef8d5e32e1849
Bruno Spyckerelle <bruno@spyckerelle.fr>
2010-11-11 15:08:01 +0000

    Suppression des données de test radio

--- src/fr/crnan/videso3d/layers/RadioCovLayer.java
+++ src/fr/crnan/videso3d/layers/RadioCovLayer.java
@@ -16,21 +16,10 @@
 
 package fr.crnan.videso3d.layers;
 
-//import java.util.HashMap;
-//import java.util.Iterator;
-//import java.util.LinkedList;
 
-// import java.io.File;
 import java.util.ArrayList;
-// import java.util.Iterator;
-
-import fr.crnan.videso3d.formats.xml.PolygonDeserializer;
-// import fr.crnan.videso3d.formats.xstream.PolygonSerializer;
-import fr.crnan.test.data.RadioCoverageInit;
 
 import fr.crnan.videso3d.graphics.RadioCovPolygon;
-import fr.crnan.videso3d.radio.RadioDataManager;
-// import fr.crnan.videso3d.radio.RadioDirectoryReader;
 import fr.crnan.videso3d.VidesoGLCanvas;
 
 import gov.nasa.worldwind.avlist.AVKey;
@@ -39,10 +28,13 @@ import gov.nasa.worldwind.layers.Layer;
 import gov.nasa.worldwind.layers.LayerList;
 import gov.nasa.worldwind.render.airspaces.Airspace;
 
+/**
+ * 
+ * @author Mickael Papail
+ * @version 0.2
+ */
 public class RadioCovLayer extends AirspaceLayer{
 
-	//private HashMap<String, RadioCovPolygon> RadioCovHash = new HashMap<String, RadioCovPolygon>();
-	//private LinkedList<RadioCovPolygon> ActiveRadioCov = new LinkedList<RadioCovPolygon>();
 	
 	private AirspaceLayer activeRadioCov = new AirspaceLayer();
 	private ArrayList<Airspace> airspaces;
@@ -103,14 +95,10 @@ public class RadioCovLayer extends AirspaceLayer{
 	}
 	
 	public void insertAllRadioCovLayers(ArrayList <Airspace> airspacesParam){
-		try {						
-						
-			RadioCoverageInit radioCoverageInit= new RadioCoverageInit(radioCov);
-			radioCoverageInit.add(); 			
+		try {									
 			
 			AirspaceLayer airspaceLayer = new AirspaceLayer();			
-			airspaces = airspacesParam;			
-			//airspaces.addAll(airspacesParam);							
+			airspaces = airspacesParam;								
 			
 			airspaceLayer.addAirspaces(airspaces);
 			airspaceLayer.setName(radioCov);
@@ -124,93 +112,6 @@ public class RadioCovLayer extends AirspaceLayer{
 		this.firePropertyChange(AVKey.LAYER, null, this);
 	}
 	
-
-	public void insertAllRadioCovLayers(String path) {
-		try {
-			
-			RadioCoverageInit radioCoverageInit= new RadioCoverageInit(radioCov);
-			radioCoverageInit.add(); 			
-			
-			AirspaceLayer airspaceLayer = new AirspaceLayer();			
-			try {
-				RadioDataManager radioDataManager = new RadioDataManager();
-				airspaces = radioDataManager.loadData();
-				//PolygonDeserializer polygonDeserializer = new PolygonDeserializer();
-				//airspaces =  polygonDeserializer.Deserialize("e:/radioCoverageData/radioOutput.xml");	 								
-				airspaceLayer.addAirspaces(airspaces);
-			}
-			catch (Exception e) {e.printStackTrace();}
-						
-			airspaceLayer.setName(radioCov);				
-			activeRadioCov = airspaceLayer;
-			layers=wwd.getModel().getLayers();				
-			layers.add(activeRadioCov);
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}																
-	}
-
-	
-	/**Insertion des Layers couvertures Radio au chargement ou lors d'une resélection dans le databaseManager */
-	public void insertAllRadioCovLayers() {
-		
-		AirspaceLayer airspaceLayer = new AirspaceLayer();
-		
-		ArrayList<Airspace> airspaceTest = null;				
-		RadioCoverageInit radioCoverageInit= new RadioCoverageInit(radioCov);
-		radioCoverageInit.add(); 								
-		/*************************  Serializer  *****************************************/
-		//PolygonSerializer polygonSerializer = new PolygonSerializer();
-		//polygonSerializer.Serialize(radioCoverageInit.getAirspaces(),"e:/testXML.xml");
-		
-		/******************* Classe de Tests à supprimer *****************************/				
-//		activeRadioCov = radioCoverageInit.getAirspaceLayers();		
-				
-		try{		
-			/************************* Deserializer *************************************/	
-			PolygonDeserializer polygonDeserializer = new PolygonDeserializer();
-//			airspaceTest = polygonDeserializer.Deserialize("e:/testXML.xml");
-			airspaceTest = polygonDeserializer.Deserialize("e:/radioCoverageData/radioOutput.xml");
-			airspaceLayer.addAirspaces(airspaceTest);
-		}
-		
-		
-		
-//			airspaceTest = polygonDeserializer.Deserialize("/media/STOREX/radioOutput.xml");
-//			activeRadioCov.addAirspaces(airspaceTest);
-
-			
-//			RadioDataManager radioDataManager = new RadioDataManager(new File(radioCovPath));
-
-			// if (radioDataManager.loadData()) {activeRadioCov.addAirspaces(radioDataManager.getAirspaces());}															  		 		 		  	
-	
-		catch (Exception e) {e.printStackTrace();}
-			
-		
-		// System.out.println("No")
-		
-		 // RadioDirectoryReader radioDirectoryReader = new RadioDirectoryReader(radioCovPath);
-		 // radioDirectoryReader.scanDirectoriesList(new File(radioCovName));												
-		
-		// activeRadioCov.addAirspaces(airspaceTest);
-		// activeRadioCov = airspaceLayer;
-		
-		
-		try {
-			// RadioDataManager radioDataManager = new RadioDataManager(path);			
-			//radioDataManager.loadData();
-			//activeRadioCov.addAirspaces(radioDataManager.getAirspaces());
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}		
-		airspaceLayer.setName(radioCov);
-		activeRadioCov=airspaceLayer;
-		layers=wwd.getModel().getLayers();
-		layers.add(activeRadioCov);		
-		this.firePropertyChange(AVKey.LAYER, null, this);
-	}
 	
 	/** Vidage de les liste des couvertures radios du layer radioCovLayer, et suppression du radioCovLayer */
 	public void removeAllRadioCovLayers() {
