4eca0039c876ece9026f70b2bb4f4abab9d8c833
Bruno Spyckerelle <bruno@spyckerelle.fr>
2010-11-21 18:03:54 +0000

    Correction bug : le context n'était plus affiché suite à un double clic

--- src/fr/crnan/videso3d/ihm/MainWindow.java
+++ src/fr/crnan/videso3d/ihm/MainWindow.java
@@ -219,7 +219,7 @@ public class MainWindow extends JFrame {
 		mainPane.setPreferredSize(new Dimension(600, 0));
 		
 		
-//		wwd.addSelectListener(context);
+		wwd.addSelectListener(context);
 		final AirspaceListener airspaceListener = new AirspaceListener(wwd, context);
 		wwd.addSelectListener(airspaceListener);
 		context.setMinimumSize(new Dimension(0,0)); //taille mini à 0 pour permettre la fermeture du panneau avec setDividerLocation
