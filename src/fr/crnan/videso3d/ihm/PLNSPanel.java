1f5963e0d73a4eefb73c04d173a2da72067b642c
Bruno Spyckerelle <bruno@spyckerelle.fr>
2012-03-18 18:46:31 +0000

    Issue 250 Analyse des fichiers plns avec une base STPV

---
+++ src/fr/crnan/videso3d/ihm/PLNSPanel.java
@@ -0,0 +1,61 @@
+/*
+ * This file is part of ViDESO.
+ * ViDESO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ViDESO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ViDESO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.crnan.videso3d.ihm;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+
+import fr.crnan.videso3d.formats.plns.PLNSAnalyzer;
+
+/**
+ * Fenêtre d'analyse d'une base PLNS
+ * @author Bruno Spyckerelle
+ * @version 0.1.0
+ */
+public class PLNSPanel extends ResultPanel {
+
+	private PLNSAnalyzer plnsAnalyzer;
+	
+	/**
+	 * 
+	 * @param path Chemin vers la base de données
+	 */
+	public PLNSPanel(String path){
+		plnsAnalyzer = new PLNSAnalyzer(path);
+		CategoryDataset dataset = plnsAnalyzer.getCategoryCodesRepartition();
+		JFreeChart chart = ChartFactory.createBarChart("Répartition de l'utilisation des codes par catégorie", "Catégorie", "Total", dataset, PlotOrientation.VERTICAL, false, true, false);
+		
+		ChartPanel panel = new ChartPanel(chart);
+		
+		this.add(panel);
+	}
+	
+	@Override
+	public void setContext(ContextPanel context) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getTitleTab() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
