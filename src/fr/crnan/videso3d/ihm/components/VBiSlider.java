1cc571a6763dc92d024ad1087ec918cebef75c86
Bruno Spyckerelle <bruno@spyckerelle.fr>
2012-05-07 10:12:48 +0000

    Ajout de la licence

--- src/fr/crnan/videso3d/ihm/components/VBiSlider.java
+++ src/fr/crnan/videso3d/ihm/components/VBiSlider.java
@@ -1,3 +1,18 @@
+/*
+ * This file is part of ViDESO.
+ * ViDESO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ViDESO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ViDESO.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.crnan.videso3d.ihm.components;
 
 import java.awt.Color;
@@ -5,14 +20,11 @@ import java.awt.Color;
 import gov.nasa.worldwind.render.airspaces.Curtain;
 import com.visutools.nav.bislider.*;
 import fr.crnan.videso3d.graphics.RadioCovPolygon;
-import fr.crnan.videso3d.ihm.RadioCovView;
-import fr.crnan.videso3d.radio.RadioCovController;
 
 
 /**  
  * @author Mickael Papail
-**/
-
+ */
 public class VBiSlider extends BiSlider {
 
 	private Boolean DEBUG = Boolean.FALSE;
