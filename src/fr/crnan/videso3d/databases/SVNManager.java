d2b547696ef02042f7b87581d9ddbb76f4f93022
Bruno Spyckerelle <bruno@spyckerelle.fr>
2013-03-22 10:22:19 +0000

    Ajout des licences

--- src/fr/crnan/videso3d/databases/SVNManager.java
+++ src/fr/crnan/videso3d/databases/SVNManager.java
@@ -1,3 +1,19 @@
+/*
+ * This file is part of ViDESO.
+ * ViDESO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ViDESO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ViDESO.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package fr.crnan.videso3d.databases;
 
 import java.io.ByteArrayOutputStream;
@@ -26,7 +42,11 @@ import org.tmatesoft.svn.core.wc.SVNWCUtil;
 
 import fr.crnan.videso3d.DatasManager;
 import fr.crnan.videso3d.ihm.DatabaseManagerUI;
-
+/**
+ * 
+ * @author Adrien Vidal
+ * @version 0.1.0
+ */
 public class SVNManager {
 
 	
--- src/fr/crnan/videso3d/ihm/SVNRepositoryUI.java
+++ src/fr/crnan/videso3d/ihm/SVNRepositoryUI.java
@@ -1,3 +1,18 @@
+/*
+ * This file is part of ViDESO.
+ * ViDESO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ViDESO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ViDESO.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.crnan.videso3d.ihm;
 
 import javax.swing.JDialog;
@@ -23,10 +38,11 @@ import java.awt.BorderLayout;
 
 /**
  * Fenêtre affichant l'arboresence du dépôt SVN sélectionné
- * @author vidal
+ * @author Adrien Vidal
  *
  */
 public class SVNRepositoryUI extends JDialog{
+	
 	private static final long serialVersionUID = -2707712944901661771L;
 	private Color background = new Color(230,230,230);
 
