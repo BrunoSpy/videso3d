e1a0e4a73a5962f1c535b2f7f366e7775bdd7d9e
bruno@spyckerelle.fr <bruno@spyckerelle.fr@b624ef8a-b9c0-11de-b017-a9a2c7051f87>
2013-07-16 15:16:20 +0000

    Merge fix import AIP

--- branches/0.12/src/fr/crnan/videso3d/Videso3D.java
+++ branches/0.12/src/fr/crnan/videso3d/Videso3D.java
@@ -35,7 +35,7 @@ import gov.nasa.worldwind.util.Logging;
  */
 public class Videso3D {
 
-	public static final String VERSION = "0.12.0.1";
+	public static final String VERSION = "0.12.0.2";
 	
 	public static void main(final String[] args)
 	{
